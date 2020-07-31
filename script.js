fc9e76b503c0dfd4c8aee936ab3f846314edb948
EVI_BREUKERS <evibreukers@gmail.com>
2020-07-31 15:55:00 +0200

    update july 31

---
+++ script.js
@@ -0,0 +1,88 @@
+/* HIDE NAV WHILE SCROLLING */
+
+let isScrolling;
+const selectNav = document.querySelectorAll("header");
+
+// Listen for scroll events
+window.addEventListener(
+  "scroll",
+  () => {
+    for (let i = 0; i < selectNav.length; i++) {
+      selectNav[i].setAttribute("style", "display:none");
+    }
+
+    // Clear our timeout throughout the scroll
+    window.clearTimeout(isScrolling);
+
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(() => {
+      // Run the callback
+      for (let i = 0; i < selectNav.length; i++) {
+        selectNav[i].setAttribute("style", "display:flex");
+      }
+    }, 800);
+  },
+  false
+);
+
+/* AOS ANIMATE SCROLL */
+AOS.init({
+  duration: 1000,
+});
+
+/* ROTATING WORDS */
+var words = document.getElementsByClassName("word");
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw =
+    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = "letter behind";
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+
+  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function () {
+    cw[i].className = "letter out";
+  }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function () {
+    nw[i].className = "letter in";
+  }, 340 + i * 80);
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = "";
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement("span");
+    letter.className = "letter";
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
